60602c68531e34618d37e2d5eb9d33fcc4290038
Tharika Dahanayake <tharikad2002@gmail.com>
2024-12-27 13:09:01 +0530

    initial commit

---
+++ web/Login/firebaseauth.js
@@ -0,0 +1,27 @@
+/*
+   //<script type ="module" src="firebaseauth.js"></script>
+  
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+  import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+  import {getFirestore, setDoc, doc} from  "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAX-suYFhRF6svCQaRXEXIFs2EF4SN_-Ag",
+    authDomain: "admin-website-cc58a.firebaseapp.com",
+    projectId: "admin-website-cc58a",
+    storageBucket: "admin-website-cc58a.appspot.com",
+    messagingSenderId: "1038815370367",
+    appId: "1:1038815370367:web:27be85a2304aef6056b34a"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  const signUp=document.getElementById('sumbitSignUp');
+  signUp.addEventListener('click',(event)=>{
+    event.preventDefault();
+
+  })
+*/
